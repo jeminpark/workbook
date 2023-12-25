8d26cda76887586d4cc7fd484bdd47850ecaf930
jeminPARK <abc@abc.com>
2023-12-25 22:50:14 +0900

    워크북에서 설정한 Gradle 설정대로 수정, gradle로 JDBC 의존성 추가한후 JUnit 테스트

---
+++ jdbcex/src/test/java/org/zerock/dao/ConnectTests.java
@@ -0,0 +1,19 @@
+package org.zerock.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConnectTests {
+
+	@Test
+	public void Test1() {
+		
+		int v1 = 10;
+		int v2 = 10;
+		
+		Assertions.assertEquals(v1,v2);
+		
+		
+	}
+	
+}
