bde823f83bb1b31632ca988b926459815421f094
JeminPARK <JeminPARK@jm.com>
2024-01-05 00:38:49 +0900

    TodoService에 db에 등록된 데이터 list로 출력 구현 학습, register 페이지 통해 값 등록시 redirect 로 list페이지 전환까지 구현 및 학습

--- jdbcex/src/main/java/org/zerock/jdbcex/controller/TodoListController.java
+++ jdbcex/src/main/java/org/zerock/jdbcex/controller/TodoListController.java
@@ -1,6 +1,7 @@
 package org.zerock.jdbcex.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.zerock.jdbcex.dto.TodoDTO;
 import org.zerock.jdbcex.service.TodoService;
 
 import lombok.extern.log4j.Log4j2;
@@ -18,11 +20,23 @@ public class TodoListController extends HttpServlet{
 	
 	private TodoService todoService = TodoService.INSTANCE;
 	
+	
+	// HttpServletRequest 의 setAttribute()를 이용해서 TodoService 객체가 반환하는 데이터를 저장하고 RequestDispatcher 를 이용해 JSP로 전달한다.
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		
 		log.info("todo list.............");
+		
+		try {
+			List<TodoDTO> dtoList = todoService.listAll();
+			req.setAttribute("dtoList", dtoList);
+			req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
+			
+		} catch(Exception e) {
+			log.error(e.getMessage());
+			throw new ServletException("list error");
+		}
 	}
 
 }
--- jdbcex/src/main/java/org/zerock/jdbcex/service/TodoService.java
+++ jdbcex/src/main/java/org/zerock/jdbcex/service/TodoService.java
@@ -1,5 +1,8 @@
 package org.zerock.jdbcex.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.zerock.jdbcex.dao.TodoDAO;
 import org.zerock.jdbcex.domain.TodoVO;
@@ -28,6 +31,9 @@ public enum TodoService {
 		
 	}
 	
+	// Todo의 등록기능은 GET방식으로 등록화면을보고 <form> 태그내에 입력 항목 들을 채운후에 POST 방식으로 처리한다.
+	// 처리 후에는 목록화면으로 redirect하는 PRG패턴 방식이다.	
+	// register() 는 파라미터로 TodoDTO 객체를 받아서 TodoVO로 변환하고 이를 저장한다.
 	public void register(TodoDTO todoDTO) throws Exception {
 		
 		TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
@@ -36,6 +42,25 @@ public enum TodoService {
 		log.info(todoVO);
 		
 		dao.insert(todoVO); // int를 반환 하므로 이를 이용해서 예외처리도 가능
+	}
+	
+	
+	// TodoDAO 에서 가져온 TodoVO 의 목록을 모두 TodoDTO로 변환해서 반환해야한다.
+	// 이떄 ModelMapper와 Java Stream 의 map()을 이용하면 간단하게 처리할수 있다.
+	public List<TodoDTO> listAll() throws Exception {
+		
+		List<TodoVO> voList = dao.selectAll();
+		
+		log.info("voList...............");
+		
+		log.info(voList);
+		
+		List<TodoDTO> dtoList = voList.stream()
+				.map(vo -> modelMapper.map(vo, TodoDTO.class))
+				.collect(Collectors.toList());
+		
+		return dtoList;
+				
 	}
 
 }
